ea1214c5f153e7893272831686d52893de8853df
DINSHA ZAFAR <95676736+Dinshazafar@users.noreply.github.com>
2023-01-07 23:45:05 +0530

    Update menudriven-compound.java

--- menudriven-compound.java
+++ menudriven-compound.java
@@ -1,4 +1,4 @@
-package com.example.menudriven;
+package com.example.intrest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,34 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class Compund extends AppCompatActivity {
-    EditText C_principle,C_interestrate,C_time;
-    Button C_button;
-    TextView C_result;
+public class Compoundintrest extends AppCompatActivity {
+    EditText e1,e2,e3;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compund);
-
-        C_principle=findViewById(R.id.principle);
-        C_interestrate=findViewById(R.id.interestrate);
-        C_time=findViewById(R.id.time);
-        C_button=findViewById(R.id.button);
-        C_result=findViewById(R.id.result);
-
-        C_button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_compoundintrest);
+        e1=(EditText) findViewById(R.id.t1);
+        e2=(EditText) findViewById(R.id.t2);
+        e3=(EditText) findViewById(R.id.t3);
+        b2=(Button) findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int p,r,t;
-                p=Integer.parseInt(C_principle.getText().toString());
-                r=Integer.parseInt(C_interestrate.getText().toString());
-                t=Integer.parseInt(C_time.getText().toString());
-                int res=(p*t*r)/100;
-                C_result.setText("Result :" + res);
+                double num1=Double.parseDouble(e1.getText().toString());
+                double num2=Double.parseDouble(e2.getText().toString());
+                double num3=Double.parseDouble(e3.getText().toString());
+                double result=Math.pow(1+num3/100,num2);
 
+                double A = num1*result;
+                b2.setText(String.valueOf(A));
             }
         });
     }
